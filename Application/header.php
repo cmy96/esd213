c15ea2e4d4bc6b821819365e75d4ee6df6dd9af6
Tanny Lai <tanny.lai.2017@sis.smu.edu.sg>
2019-03-24 18:05:27 +0800

    Salon Images

--- Application/header.php
+++ Application/header.php
@@ -13,8 +13,8 @@
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="css/shop-homepage.css" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="css/shop-homepage.css" rel="stylesheet">
 
 </head>
 
